b7cef4f1bb56db0fda6362f797e9414549a98b4c
nardi <felipebbnn@gmail.com>
2025-12-05 10:18:02 -0500

    simplyfy 5p2 code

--- 2025/5p2/main.py
+++ 2025/5p2/main.py
@@ -10,16 +10,14 @@ for line in lines_iter:
     if not line:
         break
     ranges.append([ int(x) for x in line.split('-') ])
+
+# merge ranges
 ranges.sort()
-
-ingredients = list(int(x) for x in lines_iter)
-
 m_ranges = [ranges[0]]
 for r in ranges[1:]:
     if r[0] <= m_ranges[-1][1]:
         m_ranges[-1][1] = max(m_ranges[-1][1], r[1])
     else:
         m_ranges.append(r)
-ranges = m_ranges
 
-print(sum(b-a+1 for a, b in ranges))
+print(sum(b-a+1 for a, b in m_ranges))
