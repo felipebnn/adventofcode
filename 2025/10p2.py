8535213b3d01def2d9535877b38dab97195b5aeb
nardi <felipebbnn@gmail.com>
2025-12-15 16:49:43 -0500

    simplify 10p2

--- 2025/10p2.py
+++ 2025/10p2.py
@@ -4,21 +4,21 @@ import pulp
 with open('10_input.txt', 'r') as f:
     txt = f.read().strip('\n')
 
-def solve_min_sum_linear_system(matrix, joltages):
+def solve_min_sum_linear_system(buttons, joltages):
     p = pulp.LpProblem()
 
     # bounded decision variables - [0, max_joltage]
     variables = [
         pulp.LpVariable(str(i), lowBound=0, upBound=max(joltages), cat=pulp.LpInteger)
-        for i in range(len(matrix[0]))
+        for i in range(len(buttons))
     ]
 
     # add objective to be minimized - the sum of the variables
     p += pulp.lpSum(variables)
 
     # add constraints for each linear equation
-    for coefficients, joltage in zip(matrix, joltages):
-        equation = pulp.lpSum(c * x for c, x in zip(coefficients, variables))
+    for i, joltage in enumerate(joltages):
+        equation = pulp.lpSum(x for b, x in zip(buttons, variables) if i in b)
         p += equation == joltage
 
     p.solve(pulp.PULP_CBC_CMD(msg=0))
@@ -35,11 +35,7 @@ for machine_str in txt.split('\n'):
     joltages = [
         int(j) for j in joltages[1:-1].split(',')
     ]
-    matrix = [
-        [ i in button for button in buttons ]
-        for i in range(len(joltages))
-    ]
 
-    total += solve_min_sum_linear_system(matrix, joltages)
+    total += solve_min_sum_linear_system(buttons, joltages)
 
 print(total)
